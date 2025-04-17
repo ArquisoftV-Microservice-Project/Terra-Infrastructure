ed0dd01d1807a880b28e9f4c0cc121397550e90c
Ricardo965 <ricardiox2000@gmail.com>
2025-04-17 18:40:48 -0500

    fix: changed zone due to resource availability and added docs

--- main.tf
+++ main.tf
@@ -1,52 +1,54 @@
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  credentials = file(var.credentials_file)
+  project     = var.project_id
+  region      = var.region
 }
 
 data "google_client_config" "default" {}
 
 module "network" {
-  source = "./modules/network"
+  source     = "./modules/network"
   project_id = var.project_id
-  region = var.region
+  region     = var.region
 }
 
 module "artifact_registry" {
-  source     = "./modules/artifact_registry"
-  region     = var.region
-  project_id = var.project_id
-  repo_name  = var.repo_name
+  source           = "./modules/artifact_registry"
+  region           = var.region
+  project_id       = var.project_id
+  repo_name        = var.repo_name
   repo_description = var.repo_description
 }
 
 module "cluster" {
-  source     = "./modules/cluster"
-  region     = var.region
-  project_id = var.project_id
-  vpc_name   = module.network.vpc_name
-  subnet_name = module.network.subnet_name
+  source         = "./modules/cluster"
+  region         = var.region
+  node_locations = var.node_locations
+  project_id     = var.project_id
+  vpc_name       = module.network.vpc_name
+  subnet_name    = module.network.subnet_name
 }
 
 provider "kubernetes" {
-  host  = "https://${module.cluster.cluster_endpoint}"
-  token = data.google_client_config.default.access_token
+  host                   = "https://${module.cluster.cluster_endpoint}"
+  token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.cluster.cluster_ca)
 }
 
 provider "helm" {
   kubernetes = {
-    host  = module.cluster.cluster_endpoint
-    token = data.google_client_config.default.access_token
+    host                   = module.cluster.cluster_endpoint
+    token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(module.cluster.cluster_ca)
   }
 }
 
 module "node_pools" {
-  source       = "./modules/node_pools"
-  region       = var.region
-  project_id   = var.project_id
+  source             = "./modules/node_pools"
+  region             = var.region
+  project_id         = var.project_id
   stable_gke_version = module.cluster.stable_gke_version
-  cluster_name = module.cluster.cluster_name
+  cluster_name       = module.cluster.cluster_name
 }
 
 module "namespaces" {
@@ -54,6 +56,6 @@ module "namespaces" {
 }
 
 module "ingress" {
-  source = "./modules/ingress"
+  source    = "./modules/ingress"
   namespace = "frontend"
 }
